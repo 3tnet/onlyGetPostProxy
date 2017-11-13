5d459fb148308eeb11900b963e7f4fc3f4cf7c37
taowei <t888y@qq.com>
2017-11-14 00:59:19 +0800

    first commit

---
+++ conf/conf.go
@@ -0,0 +1,37 @@
+package conf
+
+import (
+	"encoding/json"
+	"os"
+	"log"
+)
+
+type Config struct {
+	ListenPort string `json:"listen_port"`
+	ServerHost string `json:"server_host"`
+	MethodKey  string `json:"method_key"`
+	fileName   string
+}
+func NewConfig() *Config{
+	// 默认配置
+	return &Config{"9090", "127.0.0.1:8080", "_method", "./onlyGetPostProxyConf.json"}
+}
+func (c *Config) SetFileName(fileName string){
+	c.fileName = fileName
+}
+func (c *Config) ReadConfig(){
+
+	if _, err := os.Stat(c.fileName); !os.IsNotExist(err) {
+		file, err := os.Open(c.fileName)
+		if err != nil {
+			log.Fatalf("打开配置文件 %s 出错:%s", c.fileName, err)
+		}
+		defer file.Close()
+
+		err = json.NewDecoder(file).Decode(c)
+
+		if err != nil {
+			log.Fatalf("格式有误的 JSON 配置文件:\n%s", file)
+		}
+	}
+}
---
+++ onlyGetPostProxy.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"onlyGetPostProxy/conf"
+)
+var config = conf.NewConfig()
+
+func NewHostsReverseProxy(target url.URL) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		// 获取_methods参数
+		queryForm, err := url.ParseQuery(req.URL.RawQuery)
+		if err == nil && len(queryForm[config.MethodKey]) > 0 {
+			req.Method = strings.ToUpper(queryForm[config.MethodKey][0])
+		}
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+func main() {
+	config.ReadConfig()
+
+
+	proxy := NewHostsReverseProxy(url.URL{
+		Scheme: "http",
+		Host:   config.ServerHost,
+	})
+	log.Fatal(http.ListenAndServe(":" + config.ListenPort, proxy))
+}
